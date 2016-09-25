c4c3e2a1795845c0eac9c10a6b72cc57b6aef90e
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-09-26 00:28:49 +0200

    fixes math block rendering

--- mathics/builtin/inout.py
+++ mathics/builtin/inout.py
@@ -1785,7 +1785,7 @@ class MathMLForm(Builtin):
         # mathml = '<math><mstyle displaystyle="true">%s</mstyle></math>' % xml
         # #convert_box(boxes)
 
-        result = '<math><mstyle mathvariant="sans-serif">%s</mstyle></math>' % xml
+        result = '<math display="block"><mstyle mathvariant="sans-serif">%s</mstyle></math>' % xml
 
         return Expression('RowBox', Expression('List', String(result)))
 
